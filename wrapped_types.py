53f0cd6c703096a69ce41b26257c97341ff9a03d
Darren Struthers <dstruthers@gmail.com>
2017-01-30 12:15:13 -0800

    Added support for several additional special methods. Replaced "wrap" with "wrapped".

--- wrapped_types.py
+++ wrapped_types.py
@@ -49,9 +49,37 @@ class WrapperFactory(object):
                 def __and__(self, other):
                     return self._convert(self._value & other)
 
+            if hasattr(type_, '__coerce__'):
+                def __coerce__(self, other):
+                    return (self._value, type_(other))
+
+            if hasattr(type_, '__cmp__'):
+                def __cmp__(self, other):
+                    return cmp(self._value, other)
+
             if hasattr(type_, '__complex__'):
                 def __complex__(self):
                     return complex(self._value)
+
+            if hasattr(type_, '__contains__'):
+                def __contains__(self, item):
+                    return item in self._value
+
+            if hasattr(type_, '__del__'):
+                def __del__(self):
+                    del self._value
+
+            if hasattr(type_, '__delitem__'):
+                def __delitem__(self, key):
+                    del self._value[key]
+
+            if hasattr(type_, '__delslice__'):
+                def __delslice__(self, i, j):
+                    del self._value[i:j]
+
+            if hasattr(type_, '__divmod__'):
+                def __divmod__(self, other):
+                    return divmod(self._value, other)
 
             if hasattr(type_, '__div__'):
                 def __div__(self, other):
@@ -113,6 +141,10 @@ class WrapperFactory(object):
                 def __imul__(self, other):
                     self._value *= other
 
+            if hasattr(type_, '__index__'):
+                def __index__(self):
+                    return self._convert(self._value.__index__())
+
             if hasattr(type_, '__int__'):
                 def __int__(self):
                     return int(self._value)
@@ -139,6 +171,14 @@ class WrapperFactory(object):
             if hasattr(type_, '__isub__'):
                 def __isub__(self, other):
                     self._value -= other
+
+            if hasattr(type_, '__iter__'):
+                def __iter__(self):
+                    return iter(self._value)
+
+            if hasattr(type_, '__itruediv__'):
+                def __itruediv__(self, other):
+                    return self._convert(other / self._value)
 
             if hasattr(type_, '__ixor__'):
                 def __ixor__(self, other):
@@ -203,10 +243,38 @@ class WrapperFactory(object):
                     else:
                         return self._convert(self._value ** other)
                 
+            if hasattr(type_, '__radd__'):
+                def __radd__(self, other):
+                    return self._convert(other + self._value)
+
+            if hasattr(type_, '__rand__'):
+                def __rand__(self, other):
+                    return self._convert(other & self._value)
+
+            if hasattr(type_, '__rdiv__'):
+                def __rdiv__(self, other):
+                    return self._convert(other / self._value)
+
             if hasattr(type_, '__repr__'):
                 def __repr__(self):
                     return repr(self._value)
-                
+
+            if hasattr(type_, '__reversed__'):
+                def __reversed__(self):
+                    return reversed(self._value)
+
+            if hasattr(type_, '__rdivmod__'):
+                def __rdivmod__(self, other):
+                    return self._convert(divmod(other, self._value))
+
+            if hasattr(type_, '__rfloordiv__'):
+                def __rfloordiv__(self, other):
+                    return self._convert(other // self._value)
+
+            if hasattr(type_, '__rlshift__'):
+                def __rlshift__(self, other):
+                    return self._convert(other << self._value)
+
             if hasattr(type_, '__rmod__'):
                 def __rmod__(self, other):
                     return self._convert(other % self._value)
@@ -215,9 +283,41 @@ class WrapperFactory(object):
                 def __rmul__(self, other):
                     return self._convert(other * self._value)
 
+            if hasattr(type_, '__ror__'):
+                def __ror__(self, other):
+                    return self._convert(other | self._value)
+                
+            if hasattr(type_, '__rpow__'):
+                def __rpow__(self, other):
+                    return self._convert(other ** self._value)
+                
+            if hasattr(type_, '__rrshift__'):
+                def __rrshift__(self, other):
+                    return self._convert(other >> self._value)
+                
             if hasattr(type_, '__rshift__'):
                 def __rshift__(self, other):
                     return self._convert(self._value >> other)
+
+            if hasattr(type_, '__rsub__'):
+                def __rsub__(self, other):
+                    return self._convert(other - self._value)
+
+            if hasattr(type_, '__rtruediv__'):
+                def __rtruediv__(self, other):
+                    return self._convert(other / self._value)
+
+            if hasattr(type_, '__rxor__'):
+                def __rxor__(self, other):
+                    return self._convert(other ^ self._value)
+
+            if hasattr(type_, '__setitem__'):
+                def __setitem__(self, key, value):
+                    self._value[key] = value
+
+            if hasattr(type_, '__setslice__'):
+                def __setslice__(self, i, j, sequence):
+                    self._value[i:j] = sequence
 
             if hasattr(type_, '__str__'):
                 def __str__(self):
@@ -227,6 +327,10 @@ class WrapperFactory(object):
                 def __sub__(self, other):
                     return self._convert(self._value - other)
                 
+            if hasattr(type_, '__truediv__'):
+                def __truediv__(self, other):
+                    return self._convert(self._value / other)
+
             if hasattr(type_, '__unicode__'):
                 def __unicode__(self):
                     return unicode(self._value)
@@ -245,6 +349,11 @@ class WrapperFactory(object):
                         return getattr(self._value, attr)
                 else:
                     raise AttributeError('"{}" object has no attribute "{}"'.format(self.__class__.__name__, attr))
+        Wrapped.__name__ = 'Wrapped_' + type_.__name__
         return Wrapped
 
-wrap = WrapperFactory()
+_wrapped_types = WrapperFactory()
+
+def wrapped(cls):
+    """Return wrapped version of the provided class."""
+    return _wrapped_types(cls)
